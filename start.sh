0768b5e30806a48bcde16fa17899bf542370f485
Seongho Park <qkrtjdgh751014@gmail.com>
2025-11-17 23:39:07 +0900

    fix: start.sh에서 production settings 사용하도록 수정
    - DJANGO_SETTINGS_MODULE=config.settings.production 환경변수 추가
    - collectstatic이 올바른 STATIC_ROOT 설정 사용
    - ImproperlyConfigured 오류 해결

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Railway startup script
 
+# Set production settings
+export DJANGO_SETTINGS_MODULE=config.settings.production
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
